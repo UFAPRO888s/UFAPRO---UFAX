1daa4b975b3dbb3a497ba83c53421f5406f917c3
lottostock <lottostock.pnck@gmail.com>
2021-08-01 23:31:46 +0700

    ww

--- _pages/score.md
+++ _pages/score.md
@@ -1,15 +1,15 @@
 ---
 layout: home
 ---
-
 <div class="container max-w-screen-md py-24 md:py-28 mx-auto px-4 sm:px-6 lg:px-8">
-<h1 class="text-4xl font-black">ผลบอลสด **แทงบอล**
-[ufax24](https://ufax24.com/  "ufax24")
-[ufapro888s](https://ufapro888s.co/ "ufapro888s")
-</h1>
-    <p class="content-text text-lg mt-6">
+<h1 class="text-4xl font-black">ผลบอลสด แทงบอล</h1>
+
     ผลบอลสด ตารางคะแนน
-    </p>
+
 <style> #iframe-score108{ margin:0px; border: 1px solid #999; } </style>
 <iframe id="iframe-score108" width="100%" height="520" frameborder="0" scrolling="no" src="https://www.score108.com/soccer/scoreAPI" ></iframe> 
+
+
 </div>
+
+[ufax24](https://ufax24.com) [ufapro888s](https://ufapro888s.co)
--- genpost.py
+++ genpost.py
@@ -2,21 +2,37 @@
 import requests
 import urllib.request
 import json
+import markdown
+from datetime import datetime
+now = datetime.now() # current date and time
 
 url = 'https://wtf.roflcopter.fr/rss-bridge/?action=display&bridge=Vk&u=public203323312&hide_reposts=on&format=Json'
 req = urllib.request.Request(url)
-
+date_time = now.strftime("%Y-%m-%d")
+print("date and time:",date_time)
 ##parsing response
 r = urllib.request.urlopen(req).read()
 cont = json.loads(r)
 counter = 0
-
+patt = "\
+--- \n\
+layout: post \n\
+--- \n\
+"
 ##parcing json
 for item in cont['items']:
     counter += 1
-    print("Title:", item['author'], "\nComments:", item['content_html'])
-    print("----")
+    print("Title:", item['author']['name'], "\Content_html:", item['content_html'])
+    print("----")   
+    with open("./_posts/"+date_time+"-"+str(item['_rssbridge']['post_id'])+".mdx", 'w') as f:
+        f.write(patt+"\n\n"+item['content_html'])
+        f.close() 
 
-##print formated
-#print (json.dumps(cont, indent=4, sort_keys=True))
+
 print("Number of titles: ", counter)
+
+# with open("some_file.txt", "r", encoding="utf-8") as input_file:
+#     text = input_file.read()
+# html = markdown.markdown(text)
+# with open("some_file.html", "w", encoding="utf-8", errors="xmlcharrefreplace") as output_file:
+#     output_file.write(html)
